3ea017c60b1139d7fc3c60ab64e255c43fb9b71e
kumkumyadav780 <Kumkumyadav8841@gmail.com>
2026-01-15 17:29:03 +0530

    Task 1:hello world python script

---
+++ hello_world.py
@@ -0,0 +1,14 @@
+# name is the variable their I stored user name
+name=input("Enter your Name :")
+print(f'My name is {name}.')
+
+# role is the variable we are storing role of user
+role=input("Enter your role on Python Developer :")
+print(f'Pyhton Developer role {role}')
+
+# date is the name of variable here we are storing date which user will give
+#It is literal sting that's why input in string
+day=int(input("Enter Today's Day :- "))
+month=input("Enter Month :")
+year=input("Enter year :")
+print(f'Today is {day}/{month}/{year}')
